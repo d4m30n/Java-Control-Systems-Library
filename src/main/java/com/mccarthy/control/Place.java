832d0af418ac568f396bfa81252fc8fedc6f92d1
Harry McCarthy <w0455rk@protonmail.com>
2020-01-13 11:26:58 +1300

    Add the beginning of pole placement

---
+++ src/main/java/com/mccarthy/control/Place.java
@@ -0,0 +1,11 @@
+package com.mccarthy.control;
+
+import org.ejml.data.Complex_F64;
+
+public class Place {
+
+    public Place(SS sys, double[] p) {
+        Complex_F64[] eiganValues = sys.pole();
+        
+    }
+}
